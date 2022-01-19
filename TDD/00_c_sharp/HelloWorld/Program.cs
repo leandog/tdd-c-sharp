bf00dbefaae45e7a119c8bcd996a8d9dd0f64b6b
Sean Kavanagh <skavanagh@cardinalcommerce.com>
2022-01-19 10:20:25 -0500

    Downgrade to .net 3.1

--- TDD/00_c_sharp/HelloWorld/Program.cs
+++ TDD/00_c_sharp/HelloWorld/Program.cs
@@ -1,95 +1,113 @@
-﻿// Inteteractive debug by clicking in the gutter
-// <--- step through the code to inspect
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
 
-// Comment
-/* 
-  Multiline Comment
-  `SHIFT+CTRL+B` to build in Visual Stuido
-*/
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Inteteractive debug by clicking in the gutter
+            // <--- step through the code to inspect
+            Console.WriteLine("Hello, World!");
 
-
-/* Types
-
-# Value Types (primitives)
-
-- bool
-- byte
-- char
-- decimal
-- double
-- float
-- int
-- long
+            // Comment
+            /* 
+              Multiline Comment
+              `SHIFT+CTRL+B` to build in Visual Stuido
+            */
 
 
-# Reference Types
+            /* Types
 
-- object
-- string
-- dynamic
+            # Value Types (primitives)
 
-
-# Empty
-
-- null
-*/
-
-
-// Implicit vs Explicit typing
-
-var i = 10; // Implicitly typed.
-int x = 10; // Explicitly typed.
-
-Console.WriteLine("i=" + i + ", x=" + x);
+            - bool
+            - byte
+            - char
+            - decimal
+            - double
+            - float
+            - int
+            - long
 
 
-// Conditionals (if) 
+            # Reference Types
 
-var isTrue = false;
-if (isTrue) {
-    Console.WriteLine("true");
-} else {
-    Console.WriteLine("false");
-}
+            - object
+            - string
+            - dynamic
 
 
-// Arrays, Lists, & Dictionaries
+            # Empty
 
-string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+            - null
+            */
 
-List<string> optionList = new List<string>
+
+            // Implicit vs Explicit typing
+
+            var i = 10; // Implicitly typed.
+            int x = 10; // Explicitly typed.
+
+            Console.WriteLine("i=" + i + ", x=" + x);
+
+
+            // Conditionals (if) 
+
+            var isTrue = false;
+            if (isTrue)
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine("false");
+            }
+
+
+            // Arrays, Lists, & Dictionaries
+
+            string[] weekDays = new string[] { "Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat" };
+
+            List<string> optionList = new List<string>
             { "AdditionalCardPersonAdressType", /* rest of elements */ };
 
-var cities = new Dictionary<string, string>(){
-	{"UK", "London, Manchester, Birmingham"},
-	{"USA", "Chicago, New York, Washington"},
-	{"India", "Mumbai, New Delhi, Pune"}
-};
+            var cities = new Dictionary<string, string>(){
+            {"UK", "London, Manchester, Birmingham"},
+            {"USA", "Chicago, New York, Washington"},
+            {"India", "Mumbai, New Delhi, Pune"}};
 
 
-// Loops
+            // Loops
 
-foreach(KeyValuePair<string, string> kvp in cities) {
-    Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
-}
+            foreach (KeyValuePair<string, string> kvp in cities)
+            {
+                Console.WriteLine("Key: {0}, Value: {1}", kvp.Key, kvp.Value);
+            }
 
 
-// Declarations & Classes
+            // Declarations & Classes
 
-var helloWorld = new HelloWorld();
-helloWorld.name = "DEV3L";
+            var helloWorld = new HelloWorld();
+            helloWorld.name = "DEV3L";
 
-Console.WriteLine(helloWorld.greeting());
+            Console.WriteLine(helloWorld.greeting());
 
-class HelloWorld
-{
-    private string message = "Hello, World!";
 
-    public string name = "TDD in C Sharp";
 
-    public string greeting()
-    {
-        return name + "\n" + message;
+        }
+
+        class HelloWorld
+        {
+            private string message = "Hello, World!";
+
+            public string name = "TDD in C Sharp";
+
+            public string greeting()
+            {
+                return name + "\n" + message;
+            }
+        }
     }
 }
