c37d3f6880546bf649daa28680877aeacad01be3
DEV3L <jus.beall@gmail.com>
2022-01-20 02:49:25 -0500

    Add remaining exercises

---
+++ TDD/06_fizz_buzz_functional/FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,29 @@
+﻿namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public List<string> Calculate(int upperBound)
+        {
+            var result = new List<string>();
+
+            for (var i = 1; i <= upperBound; i++)
+            {
+                var value = "";
+
+                if (i % 3 == 0)
+                {
+                    value = "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    value += "Buzz";
+                }
+
+                value = value.Length != 0 ? value : i.ToString();
+                result.Add(value);
+            }
+
+            return result;
+        }
+    }
+}
